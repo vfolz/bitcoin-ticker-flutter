181a01aa757110fa5dd8fb47fabd677d14ef8888
Victor Folz <victorfolz@gmail.com>
2020-03-20 13:56:10 -0700

    Again

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -21,7 +21,7 @@ class _PriceScreenState extends State<PriceScreen> {
     String selectedCurrency = 'USD';
     return Scaffold(
       appBar: AppBar(
-        title: Text('🤑 Coin Tickerssssss'),
+        title: Text('🤑 Coin Tickerssssssssss'),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
